524cb625ea278c60639cd55b2aa452b1dd3bcec3
damodara2006 <damodara2006@gmail.com>
2025-06-18 13:23:17 +0530

    Update BASE_URL to production endpoint in Admin, Home, and Item components; configure CORS for deployment

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,7 @@ const upload = multer()
 app.use(cookieParser())
 app.use(express.json())
 app.use(cors({
-    origin:["http://localhost:5173"],
+    origin:["http://localhost:5173","https://stayfinder-4zh4.onrender.com"],
     credentials:true
 }))
 export default app
